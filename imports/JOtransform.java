5ba0a3e17e6106bb20a1ad7f64499fe5685da029
Fernando Partida Milanés <ferpart@hotmail.com>
2017-02-16 21:05:41 -0600

    fix de JOptions

--- imports/JOtransform.java
+++ imports/JOtransform.java
@@ -8,11 +8,11 @@ public class JOtransform<E> implements JoptionTrans<E> {
 	private int disp=0;
 	
 	public String inputStr_JO(String data){
-		return JOptionPane.showInputDialog(null, data);
+		return JOptionPane.showInputDialog(null , data);
 	}
 	
 	public int inputInt_JO(String data){
-		read=JOptionPane.showInputDialog(null, data);
+		read=JOptionPane.showInputDialog(null , data);
 		disp=Integer.parseInt(read);
 		return disp;
 	}
